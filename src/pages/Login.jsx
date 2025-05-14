21325b39e510f90e5542520d845293278d07bcba
junsy0901 <junsy0901@gamil.com>
2025-05-14 23:16:12 +0900

    feat: navigate 확인용 코드 추가

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h2>Login</h2>
+      <button
+        onClick={() => {
+          navigate("/folder");
+        }}
+      >
+        move to folder
+      </button>
+    </div>
+  );
 };
 export default Login;
